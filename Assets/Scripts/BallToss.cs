d136033e18800a7167915f38d788fa97cebc156f
p <patrickfungdesigns@gmail.com>
2022-08-08 23:28:01 +1000

    added Line rendering, new template for serving mechanic

--- Assets/Scripts/BallToss.cs
+++ Assets/Scripts/BallToss.cs
@@ -12,29 +12,18 @@ public class BallToss : MonoBehaviour
 
     //Tosser tossControl;
     Vector2 move;
-
     public Shot currentShot;
     ShotManager shotManager;
-
-
 
     //Landmarks within ServiceBox
     Vector3 TargetPosition;
     public Transform downlineTarget; 
 
-
-
-
-    private void Awake()
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
         TargetPosition = downlineTarget.position;
         shotManager = GetComponent<ShotManager>(); 
-    }
-
-    void ballTossing()
-    {
-        rb.velocity = transform.transform.up * tossSpeed;
     }
 
     void Update()
@@ -42,40 +31,30 @@ public class BallToss : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
-            ballTossing(); 
+            Vector3 movementDirection = new Vector3(horizontalInput, verticalInput, 0);
+            movementDirection.Normalize();
+            transform.Translate(movementDirection * speed * Time.deltaTime);
+           // ballTossing(); 
         }
-        //else if (Input.GetKeyUp(KeyCode.Space))
-        //{
-        //   // rb.velocity = this.gameObject.transform.forward * serveSpeed;
-        //}
 
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        movementDirection.Normalize();
-        transform.Translate(movementDirection * speed * Time.deltaTime);
-
-        
     }
+
+    //void ballTossing()
+    //{
+    //    rb.velocity = transform.transform.up * tossSpeed;
+    //}
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.name == "ServiceBoxLine" || Input.GetKeyUp(KeyCode.Space))
+        if (other.name == "ServiceBoxLine" || Input.GetKeyUp(KeyCode.Space))
         {
+            Debug.Log("detected"); 
             Vector3 dir = downlineTarget.position - transform.position;
             this.GetComponent<Rigidbody>().velocity = dir.normalized * currentShot.hitforce + new Vector3(0, currentShot.upforce, 0);
             serveSpeed *= 2;
         }
     }
-
-    //void Grow()
-    //{
-
-    //    //if the position of the ball is <0, make localscale the ball UP
-    //    //if(this.transform.position.x <= 0.5)
-    //    //{
-    //    //    transform.localScale *= 0.4f; 
-    //    //}
-
-    //}
 }
